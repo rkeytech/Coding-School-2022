50ff52d84297f5e2ed893cda9f38dc670218536f
Timos Zacharatos <rkeytech@protonmail.com>
2022-04-11 17:45:00 +0300

    Started creating API Controllers
    * Added Customer Controller with CRUD
    * Added Employee Controller with CRUD

---
+++ Session-27/FuelStation.Blazor/Shared/CustomerViewModels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelStation.Blazor.Shared
+{
+    public class CustomerListViewModel
+    {
+        public uint ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string CardNumber { get; set; }
+    }
+    
+    public class CustomerEditViewModel
+    {
+        public uint ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string CardNumber { get; set; }
+    }
+}
