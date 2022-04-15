fbc8691f4b3842d014aaf760cf8b35376aeea524
Timos Zacharatos <rkeytech@protonmail.com>
2022-04-15 15:37:33 +0300

    Added login system for Windows Forms

--- Session-27/FuelStation.Win/FuelStation.cs
+++ Session-27/FuelStation.Win/FuelStation.cs
@@ -1,4 +1,5 @@
 ﻿using FuelStation.Blazor.Shared;
+using FuelStation.Model;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -16,20 +17,30 @@ namespace FuelStation.Win
         private readonly HttpClient _httpClient;
         private readonly UserAuthenticatedViewModel _user;
 
-        public FuelStation(UserAuthenticatedViewModel user=null)
+        public FuelStation(HttpClient httpClient, UserAuthenticatedViewModel user)
         {
             InitializeComponent();
-            _httpClient = new HttpClient();
-            _httpClient.BaseAddress = new Uri("https://localhost:7254");
+            _httpClient = httpClient;
             _user = user;
         }
 
         private void FuelStation_Load(object sender, EventArgs e)
         {
-
+            CheckUserforMenuItems();
         }
 
-        
+        private void CheckUserforMenuItems()
+        {
+            if (_user.EmployeeType == EmployeeTypeEnum.Cashier)
+            {
+                itemsToolStripMenuItem.Visible = false;
+            }
+            if (_user.EmployeeType == EmployeeTypeEnum.Staff)
+            {
+                customersToolStripMenuItem.Visible = false;
+                transactionsToolStripMenuItem.Visible = false;
+            }
+        }
 
         private void itemsToolStripMenuItem_Click(object sender, EventArgs e)
         {
